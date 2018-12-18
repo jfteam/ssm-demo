03f3b265b82e2366a7dedbf84979f10c7818b2dc
fengwenping <244160190@163.com>
2018-12-18 12:17:38 +0800

    集成dubbo功能,并能正常发布服务

--- src/main/java/org/jfteam/service/impl/UserServiceImpl.java
+++ src/main/java/org/jfteam/service/impl/UserServiceImpl.java
@@ -1,9 +1,23 @@
 package org.jfteam.service.impl;
 
+import org.jfteam.service.UserService;
+import org.jfteam.vo.UserVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
 /**
  * @description: 描述
  * @author: fengwenping
  * @date: 2018/12/14 21:34
  */
-public class UserServiceImpl {
+@Service
+public class UserServiceImpl implements UserService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    @Override
+    public void add(UserVO userVO) throws Exception {
+
+    }
 }
