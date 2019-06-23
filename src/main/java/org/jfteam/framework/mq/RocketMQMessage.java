7abffd3775bab1c095a6db0e6eef638a9f7c4372
fengwenping <244160190@163.com>
2019-06-23 21:32:45 +0800

    RocketMQ的消息实体

--- src/main/java/org/jfteam/framework/mq/RocketMQMessage.java
+++ src/main/java/org/jfteam/framework/mq/RocketMQMessage.java
@@ -3,9 +3,10 @@ package org.jfteam.framework.mq;
 import java.io.Serializable;
 
 /**
- * @description: 描述
- * @author: fengwenping
- * @date: 2018/12/14 16:36
+ * created with IntelliJ IDEA.
+ * description: RocketMQ的消息实体
+ * author:      fengwenping
+ * date:        2019/6/23 21:30
  */
 public class RocketMQMessage<T> implements Serializable {
 
@@ -21,32 +22,16 @@ public class RocketMQMessage<T> implements Serializable {
         return topic;
     }
 
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
-
     public String getTags() {
         return tags;
-    }
-
-    public void setTags(String tags) {
-        this.tags = tags;
     }
 
     public String getKey() {
         return key;
     }
 
-    public void setKey(String key) {
-        this.key = key;
-    }
-
     public T getBody() {
         return body;
-    }
-
-    public void setBody(T body) {
-        this.body = body;
     }
 
     private RocketMQMessage(Builder<T> builder) {
